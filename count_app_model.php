a522d3df22c51db95e4d6d0433af4cca98ab7059
Cake <c_came67@yahoo.co.jp>
2011-02-01 14:04:50 +0900

    add Countup Behavoir

---
+++ count_app_model.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Count Plugin for CakePHP
+ * 
+ * PHP version 5
+ *
+ * @copyright Copyright 2010, Cake. (http://trpgtools-onweb.sourceforge.jp/)
+ * @category AppModel
+ * @package  Count Plugin
+ * @version  0.1
+ * @author   Cake <cake_67@users.sourceforge.jp>
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link     https://github.com/Cake/CakePHP-Counter-Plugin
+ */
+
+class CountAppModel extends AppModel {
+
+}
+
+?>
