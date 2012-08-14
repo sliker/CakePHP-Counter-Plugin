24d6833ee461df3e1b88a2dc253d1f2cc7cc3899
David Velásquez <zero.blend@gmail.com>
2012-08-14 15:17:42 -0500

    Upload

---
+++ View/Helpers/CountHelper.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Count Plugin for CakePHP
+ * 
+ * PHP version 5
+ *
+ * @copyright Copyright 2010, Cake. (http://trpgtools-onweb.sourceforge.jp/)
+ * @category Helper
+ * @package  Count Plugin
+ * @version  0.1
+ * @author   Cake <cake_67@users.sourceforge.jp>
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link     https://github.com/Cake/CakePHP-Counter-Plugin
+ */
+class CountHelper extends Helper {
+	public $helpers = array('Html');
+
+	function totalCount($data, $options = array()) {
+
+		$total_count = 0;
+		if (isset($data['Count'][0]['Count'][0]['total_count']) && (is_string($data['Count'][0]['Count'][0]['total_count']) || is_int($data['Count'][0]['Count'][0]['total_count']))) {
+			$total_count = $data['Count'][0]['Count'][0]['total_count'];
+		} elseif (isset($data['Count']['total_count']) && (is_string($data['Count']['total_count']) || is_int($data['Count']['total_count']))) {
+			$total_count = $data['Count']['total_count'];
+		} else {
+			return null;
+		}
+
+		if (isset($options['tag'])) {
+			$name = $options['tag'];
+			unset($options['tag']);
+		} else {
+			$name = 'span';
+		}
+
+		$options = array_merge(
+			array(
+				'id' => null,
+				'class' => null,
+			),
+			(array)$options
+		);
+
+		return $this->Html->tag($name, $total_count, $options);
+	}
+
+}
